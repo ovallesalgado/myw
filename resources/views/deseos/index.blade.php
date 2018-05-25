568f29ac833d5ab2328e7bacd08c834f09513695
jhon jairo ovalles <ovallesalgado@gmail.com>
2018-05-25 16:20:49 -0500

    proteccion de rutas, vista de listar sin la logica

---
+++ resources/views/deseos/index.blade.php
@@ -0,0 +1,35 @@
+
+@extends('layouts.app')
+
+@section('content')
+
+
+<div class="container">
+<div class="row">
+
+<table class="table table-striped table-darks table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Precio</th>
+      <th scope="col">Descripción</th>
+      <th scope="col">Costo</th>
+      <th scope="col">Ahorro</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    
+  </tbody>
+</table>
+</div>
+</div>
+@endsection
