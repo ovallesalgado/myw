b040e3ba5870328b93988940bb829c1b36622e60
jhon jairo ovalles <ovallesalgado@gmail.com>
2018-05-24 21:42:34 -0500

    titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    My whims
+                    MY WHIMS
                 </div>
 
                 <div class="links">
